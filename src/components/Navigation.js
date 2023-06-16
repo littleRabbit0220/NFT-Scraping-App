e8f4213fa8097f5ca01774175282908c40585e99
littleRabbit0220 <bluecoder0220@gmail.com>
2023-06-16 22:20:45 +0300

    navigation added.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,13 @@
 const Navigation = () => {
   return (
-    <div></div>
+    <div className="flex flex-row justify-center items-center p-5">
+      <div className="text-4xl cursor-pointer mr-5 hover:text-blue-500"><i className="fa fa-angle-left"></i></div>
+      <div className="flex flex-row text-3xl itemx-center">
+        <input type="text" className="w-[50px] mr-3 border-2 rounded-xl focus:outline-none focus:border-blue-700 focus:shadow-outline" value={1}></input>
+        <h3> of 10 pages</h3>
+      </div>
+      <div className="text-4xl cursor-pointer ml-5 hover:text-blue-500"><i className="fa fa-angle-right"></i></div>
+    </div>
   )
 }
 
