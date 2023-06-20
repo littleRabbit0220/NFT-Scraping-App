c1fbf6d859177c252d731cc7a2cf7944abc2eeb0
littleRabbit0220 <bluecoder0220@gmail.com>
2023-06-20 13:08:20 +0300

    cheerio added.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 import * as cheerio from 'cheerio';
+import moment from 'moment';
 import config from '../config/index'
 
 export const getData = (date) => {
   return new Promise(async(resolve, reject) => {
-    const response = await fetch(config.cors_proxy_url + config.scraping_host_url, {
+    const response = await fetch(config.cors_proxy_url + config.scraping_host_url+'?saleDate=' + moment(date).format("YYYY")+'-'+moment(date).format("MM")+'-'+moment(date).format("DD"), {
       method: 'get',
       headers: {
         "Content-Type": "application/json"
@@ -12,6 +13,8 @@ export const getData = (date) => {
   if(response.ok) {
     const data = await response.text();
     const $ = cheerio.load(data);
+    const html = $('title').text();
+    console.log(html);
   } else {
     reject('error');
   }
